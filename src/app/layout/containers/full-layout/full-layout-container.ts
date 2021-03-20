7271e28cb32d3bdd556ac2854eb2b26cd8f487fb
Cesar Velásquez Cordova <ces.velasquez@hotmail.com>
2021-03-19 21:51:08 -0600

    initial commit

---
+++ src/app/layout/containers/full-layout/full-layout-container.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-full-layout-container',
+  templateUrl: './full-layout.container.html'
+})
+// tslint:disable-next-line: component-class-suffix
+export class FullLayoutContainer implements OnInit{ 
+
+  constructor(){}
+  
+  ngOnInit(): void {
+
+  }
+}
---
+++ src/app/layout/containers/simple-layout/simple-layout.container.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-layout-container',
+  templateUrl: './simple-layout.container.html'
+})
+// tslint:disable-next-line: component-class-suffix
+export class SimpleLayoutContainer implements OnInit{ 
+
+  constructor(){}
+  
+  ngOnInit(): void {
+
+  }
+}
